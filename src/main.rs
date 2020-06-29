aa42666ce847496b4366c85220ad3ac7c651b9de
stein-k <karlsen.stein@gmail.com>
2020-06-29 14:12:37 +0200

    Can save and restore bloom filter

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,21 @@ struct SavedBloomFilter {
 }
 
 fn main() {
-    let saved_bloom_filter = SavedBloomFilter{ number_of_hash_functions: 2, bit_vector: BitVec::from_elem(128, false)};
-    println!("hash_functions: {}, set_bits: {}", saved_bloom_filter.number_of_hash_functions, saved_bloom_filter.bit_vector.capacity());
+    let bloom_filter_to_save = SavedBloomFilter{ number_of_hash_functions: 2, bit_vector: BitVec::from_elem(128, false)};
+    println!("hash_functions: {}, capacity: {}", bloom_filter_to_save.number_of_hash_functions, bloom_filter_to_save.bit_vector.capacity());
     let mut output_file = File::create("foo.bin").expect("Unable to open file");
-    output_file.write(slice::from_ref(&saved_bloom_filter.number_of_hash_functions));
-    output_file.write(&saved_bloom_filter.bit_vector.to_bytes());
-    println!("Hello, world!");
+    output_file.write(slice::from_ref(&bloom_filter_to_save.number_of_hash_functions));
+    output_file.write(&bloom_filter_to_save.bit_vector.to_bytes());
 
     let input_file = File::open("foo.bin").expect("Unable to open input file");
     let mut reader = BufReader::new(input_file);
     let mut number_of_hash_functions = 0u8;
 
     reader.read(slice::from_mut(&mut number_of_hash_functions));
-    println!("read number of hash fuctions: {}", number_of_hash_functions)
+    let mut bytes: Vec<u8> = Vec::new();
+    reader.read_to_end(&mut bytes);
+    println!("read number of hash fuctions: {}", number_of_hash_functions);
+    let saved_bloom_filter = SavedBloomFilter{ number_of_hash_functions: number_of_hash_functions, bit_vector: BitVec::from_bytes(&bytes)};
+    println!("hash_functions: {}, capacity: {}", saved_bloom_filter.number_of_hash_functions, saved_bloom_filter.bit_vector.capacity());
 
 }
